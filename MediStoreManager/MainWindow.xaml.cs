8dde205e2712b6f8da533c94a23c6cb26c384a5f
Kevin Sherman <shermakc@mail.uc.edu>
2025-02-06 21:57:48 -0500

    create functions to retrive persons from database

--- MediStoreManager/MainWindow.xaml.cs
+++ MediStoreManager/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace MediStoreManager
 {
@@ -16,19 +17,108 @@ namespace MediStoreManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> personList;
+        List inventoryList;
+        List supplierList;
+
+        string connString = "server=localhost;uid=root;pwd=Enough@99;database=medistore manager"; // Don't leave this as plain text
+
         public MainWindow()
         {
             InitializeComponent();
+            personList = new List<Person>();
+            inventoryList = new List();
+            supplierList = new List();
+
+            try
+            {
+                MySqlConnection con = OpenMySQLConnection();
+                GetPersonList(con);
+                con.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void DataGrid_SelectionChanged()
         {
-
+            
         }
 
         private void PatientListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private Person GetPerson(MySqlDataReader reader)
+        {
+            Person tempPerson = new Person();
+            tempPerson.ID = GetColValAsUInt(reader, nameof(tempPerson.ID));
+            tempPerson.FirstName = GetColValAsString(reader, nameof(tempPerson.FirstName));
+            tempPerson.LastName = GetColValAsString(reader, nameof(tempPerson.LastName));
+            tempPerson.MiddleName = GetColValAsString(reader, nameof(tempPerson.MiddleName));
+            tempPerson.HomePhone = GetColValAsDecimal(reader, nameof(tempPerson.HomePhone));
+            tempPerson.CellPhone = GetColValAsDecimal(reader, nameof(tempPerson.CellPhone));
+            tempPerson.AddressID = GetColValAsUInt(reader, nameof(tempPerson.AddressID));
+            tempPerson.InsuranceProvider = GetColValAsString(reader, nameof(tempPerson.InsuranceProvider));
+            tempPerson.IsPatientContact = reader.GetBoolean(8);
+            tempPerson.ContactID = GetColValAsUInt(reader, nameof(tempPerson.ContactID));
+            tempPerson.ContactRelationship = GetColValAsString(reader, nameof(tempPerson.ContactRelationship));
+
+            return tempPerson;
+        }
+
+        private string GetColValAsString(MySqlDataReader reader, string colName)
+        {
+            if (reader[colName] == DBNull.Value)
+                return string.Empty;
+            else
+                return reader[colName].ToString();
+        }
+
+        private uint GetColValAsUInt(MySqlDataReader reader, string colName)
+        {
+            if (reader[colName] == DBNull.Value)
+                return 0;
+            else
+                return (uint)reader[colName];
+        }
+
+        private decimal GetColValAsDecimal(MySqlDataReader reader, string colName)
+        {
+            if (reader[colName] == DBNull.Value)
+                return 0;
+            else
+                return (decimal)reader[colName];
+        }
+
+        private DateTime GetColValAsDateTime(MySqlDataReader reader, string colName)
+        {
+            if (reader[colName] == DBNull.Value)
+                return DateTime.MinValue;
+            else
+                return (DateTime)reader[colName];
+        }
+
+        private MySqlConnection OpenMySQLConnection()
+        {
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = connString;
+            con.Open();
+            return con;
+        }
+
+        private void GetPersonList(MySqlConnection con)
+        {
+            string sql = "select * from person;";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                personList.Add(GetPerson(reader));
+            }
+        }        
     }
 }
---
+++ MediStoreManager/Tables.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MediStoreManager
+{
+    public class Address
+    {
+        int ID;
+        string StreetName;
+        int AddressNumber;
+        string City;
+        string State;
+        int ZipCode;
+    }
+
+    public class CustomerOrder
+    {
+        int ID;
+        int InventoryID;
+        string Type;
+        int PersonID;
+        int Quantity;
+        DateTime Date;
+        bool HaveReceivedPayment;
+        DateOnly PaymentDate;
+        int RelatedInventoryItemID;
+        string Notes;
+    }
+
+    public class Inventory
+    {
+        int ID;
+        string Type;
+        string Name;
+        string Size;
+        string Brand;
+        int NumInStock;
+        decimal Cost;
+        decimal RetailPrice;
+        bool IsRental;
+        decimal RentalPrice;
+        int PersonID;
+    }
+
+    public class Order
+    {
+        int ID;
+        int InventoryID;
+        int Quantity;
+        string SupplierName;
+        string ShippingMethod;
+        DateTime OrderDateTime;
+        bool HasBeenReceived;
+        DateOnly ReceivedDate;
+    }
+
+    public class Person
+    {
+        public uint ID;
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public decimal HomePhone;
+        public decimal CellPhone;
+        public uint AddressID;
+        public string InsuranceProvider;
+        public bool IsPatientContact;
+        public uint ContactID;
+        public string ContactRelationship;
+    }
+
+    public class Supplier
+    {
+        string Name;
+        decimal PhoneNumber;
+        int PartnerID;
+        int AddressID;
+    }
+
+    public class User
+    {
+        int ID;
+        string FirstName;
+        string LastName;
+        string Username;
+        string Password;
+        string Position;
+    }
+}

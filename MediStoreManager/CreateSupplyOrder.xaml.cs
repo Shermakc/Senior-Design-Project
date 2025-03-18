0a69d8b13f2f8795d88baceb6b1da60e0085224f
rodgerjk <78975122+rodgerjk@users.noreply.github.com>
2025-03-17 23:01:42 -0400

    Captures all entered values in pop-ups in variables in main class

--- MediStoreManager/CreateSupplyOrder.xaml.cs
+++ MediStoreManager/CreateSupplyOrder.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
 
 namespace MediStoreManager
 {
@@ -19,14 +21,36 @@ namespace MediStoreManager
     /// </summary>
     public partial class CreateSupplyOrder : Window
     {
+        public string Supplier { get; private set; }
+        public string InventoryID { get; private set; }
+        public string Quantity { get; private set; }
+        public string ShippingMethod { get; private set; }
+        public DateTime OrderDate { get; private set; }
+        public DateTime ReceivedDate { get; private set; }
+
         public CreateSupplyOrder()
         {
+            Supplier = "";
+            InventoryID = "";
+            Quantity = "";
+            ShippingMethod = "";
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Cancel(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            this.DialogResult = false;
+        }
+
+        private void Button_OK(object sender, RoutedEventArgs e)
+        {
+            Supplier = SupplierTextBox.Text;
+            InventoryID = InventoryTextBox.Text;
+            Quantity = QuantityTextBox.Text;
+            ShippingMethod = ShippingMethodTextBox.Text;
+            if (OrderDateDatePicker.SelectedDate.HasValue) { OrderDate = OrderDateDatePicker.SelectedDate.Value; }
+            if (ReceivedDateDatePicker.SelectedDate.HasValue) { ReceivedDate = ReceivedDateDatePicker.SelectedDate.Value; }
+            this.DialogResult = true;
         }
     }
 }
--- UnitTests/obj/Debug/net8.0-windows/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0-windows/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55edcbc3f639c426916220922cd2f8a2a3cdae56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81db5911be6d6c4d79c22b53006dfb79daed920")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

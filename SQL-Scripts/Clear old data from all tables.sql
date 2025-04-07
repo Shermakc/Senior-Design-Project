e9f214aa807622f01bd286cc3ffcc9ae1aca8bb3
Shermakc <47038995+Shermakc@users.noreply.github.com>
2025-04-06 23:15:39 -0400

    Add files via upload

--- SQL-Scripts/Clear old data from all tables.sql
+++ SQL-Scripts/Clear old data from all tables.sql
@@ -1,7 +1,7 @@
-delete from `medistore manager`.customer_order where ID < 1000;
-delete from `medistore manager`.order where ID < 1000;
-delete from `medistore manager`.inventory_item where ID < 1000;
+delete from `medistore manager`.customer_order where ID < 5000;
+delete from `medistore manager`.order where ID < 5000;
+delete from `medistore manager`.inventory_item where ID < 5000;
 delete from `medistore manager`.supplier where Name != "test";
-delete from `medistore manager`.person where ID < 1000;
-delete from `medistore manager`.address where ID < 1000;
-delete from `medistore manager`.user where ID < 1000;
+delete from `medistore manager`.person where ID < 5000;
+delete from `medistore manager`.address where ID < 5000;
+delete from `medistore manager`.user where ID < 5000;

07439dbce425f5b9023646246079b72547187ce1
rodgerjk <78975122+rodgerjk@users.noreply.github.com>
2025-04-08 00:37:08 -0400

    Fixed empty phone numbers being displayed as '0'. Now shows nothing when the value is '0'

---
+++ MediStoreManager/ZeroToEmptyStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MediStoreManager
+{
+
+    public class ZeroToEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && str == "0")
+            {
+                return string.Empty;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Optional: If user clears the TextBox, convert it back to "0"
+            if (value is string str && string.IsNullOrWhiteSpace(str))
+            {
+                return "0";
+            }
+            return value;
+        }
+    }
+}
